879a40d1f78491cc12b8dfef3e7ce3c6887140ff
Andrew Tweito <atweito@gmail.com>
2018-05-02 10:49:04 -0400

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Level-Up Tutorial Blog Example',
+    desc: "A new blog",
+    funnelName: "GatsbyFunnel"
+  },
+  pathPrefix: "/gatsby",
+  plugins: [
+    'gatsby-plugin-react-helmet', 
+    'gatsby-plugin-styled-components',
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "img",
+        path: `${__dirname}/src/images`
+      }
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "src",
+        path: `${__dirname}/src/`
+      }
+    },
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        excerpt_separator: `<!-- end -->`
+      }
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp'
+  ]
+};
---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class About extends Component {
+    render() {
+        return (
+            <div>
+                <h1>About Us</h1>
+                <p>
+                Vaporware migas poutine snackwave street art chartreuse asymmetrical, shaman synth meggings skateboard church-key. Ugh pinterest intelligentsia retro cred dreamcatcher truffaut. Narwhal four loko quinoa fanny pack, hammock occupy hashtag pop-up mixtape pabst. Stumptown cornhole keytar tumblr gochujang man bun locavore street art. Vexillologist health goth disrupt semiotics woke kitsch.
+                </p>
+            </div>
+        )
+    }
+}
